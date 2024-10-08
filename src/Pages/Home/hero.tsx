6556b01c3bb2474e1d4700ae4fa407f3139d2558
harrynurbudyana <harrynurbudyana@gmail.com>
2024-10-08 13:55:16 +0700

    update

--- src/Pages/Home/hero.tsx
+++ src/Pages/Home/hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
           <div className="max-w-5xl">
             <h1 className="mb-5 text-5xl font-bold text-white">Welcome to Torsi EV</h1>
             <p className="mb-5 text-white">
-            We provide the best services to make your life better.  Join us and start your journey with us today. We provide the best services to make your life better.  Join us and start your journey with us today.
+            Kami berusaha memberikan pelayanan terbaik. Bergabung dengan kami dan nikmati pengalaman yang berbeda.
             </p>
             <button className="btn bg-sky-400 border-transparent text-white">Get Started</button>
           </div>
