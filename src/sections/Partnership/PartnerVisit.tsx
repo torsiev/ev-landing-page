db7cfa7eab29cfad7f34c5e6ed4e2ed4b1b9ef23
salmanida <salmanidauljannah@gmail.com>
2024-11-05 14:04:34 +0700

    chore: add descriptions to photos on partnership page

--- src/sections/Partnership/PartnerVisit.tsx
+++ src/sections/Partnership/PartnerVisit.tsx
@@ -35,49 +35,45 @@ export default function Description() {
             Collaboration in Action
           </div>
 
-          <div className="flex flex-col gap-3">
-            
+          <div className="flex flex-col gap-3  pr-3 pl-3 md:p-0">
             <div className="max-w-screen-xl justify-items-center">
               <div className="flex flex-col md:flex-row gap-3">
                 <div className="relative group">
-                  <Image src={pict1} className="size-auto" alt="pict1" />
-                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
-                    <p className="text-white text-center p-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, exercitationem.</p>
+                  <Image
+                    src={pict1}
+                    className="size-auto rounded-md"
+                    alt="pict1"
+                  />
+                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity rounded-md">
+                    <p className="text-white text-center p-10">
+                    Uji coba penggunaan charging station Torsi EV -
+                      Jumat, 20 Oktober 2023
+                    </p>
                   </div>
                 </div>
                 <div className="relative group">
-                  <Image src={pict2} className="size-auto" alt="pict2" />
-                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
-                    <p className="text-white text-center">Deskripsi Gambar 2</p>
+                  <Image
+                    src={pict2}
+                    className="size-auto rounded-md"
+                    alt="pict2"
+                  />
+                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity rounded-md">
+                    <p className="text-white text-center p-10">
+                      Kunjungan mitra powerindo: Uji coba performa Stroom! dengan
+                      pemeriksaan respons throttle - Rabu, 04 Oktober 2023
+                    </p>
                   </div>
                 </div>
                 <div className="relative group">
-                  <Image src={pict3} className="size-auto" alt="pict3" />
-                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
-                    <p className="text-white text-center">Deskripsi Gambar 3</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            
-            <div className="max-w-screen-xl justify-items-center">
-              <div className="flex flex-col md:flex-row gap-3">
-                <div className="relative group">
-                  <Image src={pict4} className="size-auto" alt="pict1" />
-                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
-                    <p className="text-white text-center">Deskripsi Gambar 1</p>
-                  </div>
-                </div>
-                <div className="relative group">
-                  <Image src={pict5} className="size-auto" alt="pict2" />
-                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
-                    <p className="text-white text-center">Deskripsi Gambar 2</p>
-                  </div>
-                </div>
-                <div className="relative group">
-                  <Image src={pict6} className="size-auto" alt="pict3" />
-                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
-                    <p className="text-white text-center">Deskripsi Gambar 3</p>
+                  <Image
+                    src={pict3}
+                    className="size-auto rounded-md"
+                    alt="pict3"
+                  />
+                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity rounded-md">
+                    <p className="text-white text-center p-10">
+                    Kunjungan mitra powerindo: Kunjungan lokasi bengkel Torsi EV - Rabu, 04 Oktober 2023
+                    </p>
                   </div>
                 </div>
               </div>
@@ -86,26 +82,84 @@ export default function Description() {
             <div className="max-w-screen-xl justify-items-center">
               <div className="flex flex-col md:flex-row gap-3">
                 <div className="relative group">
-                  <Image src={pict7} className="size-auto" alt="pict1" />
-                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
-                    <p className="text-white text-center">Deskripsi Gambar 1</p>
+                  <Image
+                    src={pict4}
+                    className="size-auto rounded-md"
+                    alt="pict1"
+                  />
+                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity rounded-md">
+                    <p className="text-white text-center p-10">
+                      Foto bersama jajaran Torsi EV bersama mitra Powerindo - Rabu, 04 Oktober 2023
+                    </p>
                   </div>
                 </div>
                 <div className="relative group">
-                  <Image src={pict8} className="size-auto" alt="pict2" />
-                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
-                    <p className="text-white text-center">Deskripsi Gambar 2</p>
+                  <Image
+                    src={pict5}
+                    className="size-auto rounded-md"
+                    alt="pict2"
+                  />
+                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity rounded-md">
+                    <p className="text-white text-center p-10">
+                    Suasana rapat diskusi kerja sama bersama mitra Powerindo - Rabu, 04 Oktober 2023
+                    </p>
                   </div>
                 </div>
                 <div className="relative group">
-                  <Image src={pict9} className="size-auto" alt="pict3" />
-                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
-                    <p className="text-white text-center">Deskripsi Gambar 3</p>
+                  <Image
+                    src={pict6}
+                    className="size-auto rounded-md"
+                    alt="pict3"
+                  />
+                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity rounded-md">
+                    <p className="text-white text-center p-10">
+                    Penjelasan sistem kerja motor konversi Torsi EV kepada mitra Powerindo - Rabu, 04 Oktober 2023
+                    </p>
                   </div>
                 </div>
               </div>
             </div>
 
+            <div className="max-w-screen-xl justify-items-center">
+              <div className="flex flex-col md:flex-row gap-3">
+                <div className="relative group">
+                  <Image
+                    src={pict7}
+                    className="size-auto rounded-md"
+                    alt="pict1"
+                  />
+                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity rounded-md">
+                    <p className="text-white text-center p-10">
+                      Foto bersama jajaran Torsi EV dan mahasiswa FIT bersama mitra XXX - Jumat, 20 Oktober 2023
+                    </p>
+                  </div>
+                </div>
+                <div className="relative group">
+                  <Image
+                    src={pict8}
+                    className="size-auto rounded-md"
+                    alt="pict2"
+                  />
+                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity rounded-md">
+                    <p className="text-white text-center p-10">
+                     Pemasangan Charging Point di TULT bersama mitra XXX - Jumat, 20 Oktober 2023
+                    </p>
+                  </div>
+                </div>
+                <div className="relative group">
+                  <Image
+                    src={pict9}
+                    className="size-auto rounded-md"
+                    alt="pict3"
+                  />
+                  <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity rounded-md">
+                    <p className="text-white text-center p-10">
+                      Perakitan komponen Charging Point di TULT bersama mitra XXX - Jumat, 20 Oktober 2023
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
